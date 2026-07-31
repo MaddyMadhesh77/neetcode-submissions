60927429e2942432d3bb5ef085f0a98c9553fc7f
Madheswar R S <141992785+MaddyMadhesh77@users.noreply.github.com>
2026-08-01 00:43:31 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 1, r = x;
+
+        while (l <= r) {
+            int m = l + ((r - l) / 2);
+            if (m > x/m) {
+                r = m - 1;
+            } else if (m < x/m) {
+                l = m + 1;
+            } else {
+                return m;
+            }
+        }
+        return r;
+    }
+};
